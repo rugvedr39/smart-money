76b90152d36065759844ae470ee612de56c0a917
Rugved-Technify <rugvedr39@gmail.com>
2024-09-28 08:16:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ app.use('/payments', paymentRoutes);
 app.use('/admin', adminRoutes);
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
